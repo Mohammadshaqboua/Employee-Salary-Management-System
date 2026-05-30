21a80e56df64673d71e0c44b694704e2a844f2eb
M_Shaqboua <m.shaqboua25@gmail.com>
2026-05-30 16:07:17 +0300

    Project preparation Employee Salary Management System

---
+++ main.asm
@@ -0,0 +1,27 @@
+;====================================================================
+; Program Name: Employee Salary Management System (ESMS)
+; Program Description: A console-based Employee Salary Management System
+;              that stores up to 50 employee records, validates
+;              all inputs, calculates net salaries, displays pay
+;              slips, department statistics, and salary histogram.
+;Authors: Mohammad Shaqboua ,
+;         Mohammad Dawas ,  
+;         Husam Ziadeh
+;====================================================================
+
+INCLUDE Irvine32.inc
+.data
+    ;var
+
+    ;prompts
+
+    ;arrays
+    
+.code
+main PROC
+
+exit
+
+main ENDP
+
+End main
